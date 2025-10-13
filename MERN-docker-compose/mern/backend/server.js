7ccad149fcce6ea4cb385ec72fbbecd7a8a4ea4d
fervinod <fdsdevops8@gmail.com>
2025-10-13 23:26:28 +0800

    Added cors value

--- MERN-docker-compose/mern/backend/server.js
+++ MERN-docker-compose/mern/backend/server.js
@@ -5,6 +5,9 @@ import records from "./routes/record.js";
 const PORT = process.env.PORT || 5050;
 const app = express();
 
+const cors = require("cors");
+app.use(cors({ origin: "http://18.234.153.144:5173" }));
+
 app.use(cors());
 app.use(express.json());
 app.use("/record", records);
